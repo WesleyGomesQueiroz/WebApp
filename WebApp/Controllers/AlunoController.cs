fe4aaca96d76fc75653b483268ba24be3941cdbd
Wesley Gomes Queiroz <wgomesq@santillana.local>
2023-01-14 19:51:05 -0300

    pegar todos os alunos

---
+++ WebApp/Controllers/AlunoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AlunoController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetAll")]
+        public List<Alunos> GetAll()
+        {
+            Alunos aluno = new Alunos();
+
+            return aluno.listaAlunos();
+        }
+
+
+    }
+}
---
+++ WebApp/Models/Alunos.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace WebApp.Models
+{
+    public class Alunos
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string sobrenome { get; set; }
+        public string telefone { get; set; }
+        public int ra { get; set; }
+
+        public List<Alunos> listaAlunos()
+        {
+            Alunos aluno = new Alunos();
+            aluno.id = 1;
+            aluno.nome = "Wesley";
+            aluno.sobrenome = "Gomes Queiroz";
+            aluno.telefone = "11 958657154";
+            aluno.ra = 00001;
+            
+            Alunos aluno1 = new Alunos();
+            aluno1.id = 2;
+            aluno1.nome = "Daisy";
+            aluno1.sobrenome = "Galvão";
+            aluno1.telefone = "11 958657154";
+            aluno1.ra = 00002;
+
+            List<Alunos> listaAlunos = new List<Alunos>();
+
+            listaAlunos.Add(aluno);
+            listaAlunos.Add(aluno1);
+
+            return listaAlunos;
+        }
+    }
+}
